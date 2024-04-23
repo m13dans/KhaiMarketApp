251b035255c1649cbb30401ba96d03306545e895
Arya <danuraryapratama@gmail.com>
2024-04-23 14:23:03 +0700

    fix file structure

--- KhaiMarket.API/Products/ProductsController.cs
+++ KhaiMarket.API/Products/ProductsController.cs
@@ -4,21 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using static KhaiMarket.API.Products.GetProducts;
 
-namespace KhaiMarket.API.Products.Controllers;
+namespace KhaiMarket.API.Products;
 
 [Route("api/Products")]
 [ApiController]
 public class ProductsController : ControllerBase
 {
-    private readonly GetProductsQuery _query;
-    public ProductsController(GetProductsQuery query)
-    {
-        _query = query;
-    }
+    // private readonly GetProductsQuery _query;
+    // public ProductsController(GetProductsQuery query)
+    // {
+    //     _query = query;
+    // }
     [HttpGet]
-    public async Task<ActionResult> Get()
+    public async Task<ActionResult> Get(GetProductsQuery query)
     {
-        var products = await _query.GetProductsAsync();
+        var products = await query.GetProductsAsync();
         if (products is null )
         {
             return NotFound(products);
