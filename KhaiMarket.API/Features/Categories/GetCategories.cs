da439c00619f527cefc706194bb08610807729f3
Danur Arya <danuraryapratama@gmail.com>
2024-05-19 14:52:24 +0700

    add category controller

--- KhaiMarket.API/Features/Categories/GetCategories.cs
+++ KhaiMarket.API/Features/Categories/GetCategories.cs
@@ -1,7 +1,28 @@
+using ErrorOr;
+using KhaiMarket.API.Core.Entities;
+using KhaiMarket.API.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace KhaiMarket.API.Features.Categories
 {
     public class GetCategories
     {
+        private readonly AppDbContext _context;
+        public GetCategories(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ErrorOr<List<Category>>> GetAsync()
+        {
+            var categories = _context.Categories;
+            if (!await categories.AnyAsync())
+            {
+                return Error.NotFound();
+            }
+            var result = await categories.ToListAsync();
+            return result;
+        }
 
     }
 }
