4949b2d348aeae35072d7d9e951849d4395fe9bc
Arya <danuraryapratama@gmail.com>
2024-05-23 10:15:53 +0700

    separating search component

--- KhaiMarket.Client/src/app/components/nav-bar/nav-bar.component.ts
+++ KhaiMarket.Client/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { SearchComponent } from '../search/search.component';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, SearchComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
